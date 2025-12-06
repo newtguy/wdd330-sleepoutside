f4c1a0d4ddfe44ac5e7eeb34cb9cf923cdaf7310
newt <nate.e.shelley@gmail.com>
2025-12-06 08:04:57 -0700

    fixed vite.config.js -- aligns with current project directories

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        product_listing: resolve(__dirname, "src/product_listing/index.html"),
+        product_details: resolve(__dirname, "src/product_pages/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
+        success: resolve(__dirname, "src/checkout/success.html"), 
       },
     },
   },
